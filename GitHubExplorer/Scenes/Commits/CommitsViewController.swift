7acfa67af402161488d073d7d5431a5bb14e552e
Maciej Piechoczek <mpiechoczek@magicjack.com>
2017-12-12 11:57:14 +0100

    Structure project by scenes

---
+++ GitHubExplorer/Scenes/Commits/CommitsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CommitsViewController.swift
+//  GitHubExplorer
+//
+//  Created by Maciej Piechoczek on 12/12/2017.
+//  Copyright © 2017 McPie. All rights reserved.
+//
+
+import UIKit
+
+class CommitsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ GitHubExplorer/Scenes/Repositories/RepositoriesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RepositoriesViewController.swift
+//  GitHubExplorer
+//
+//  Created by Maciej Piechoczek on 12/12/2017.
+//  Copyright © 2017 McPie. All rights reserved.
+//
+
+import UIKit
+
+class RepositoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
